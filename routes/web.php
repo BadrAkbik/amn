62273f7eae0509c3e8d7d22b75c310f6ec701ccb
BadrAkbik <badrakbik1@gmail.com>
2024-09-15 15:48:48 +0300

    middlewares

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect('admin');
-});
+})->middleware('throttle:60,1');
 
 Route::get('/print/{report}', function ($report) {
 
     $report = Report::findOrFail($report);
     return view('print', ['report' => $report]);
-})->name('print');
+})->middleware(['auth', 'throttle:60,1'])->name('print');
