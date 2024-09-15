37b017a64f9ad5dc323cc6b56c604236b1ec48e0
BadrAkbik <badrakbik1@gmail.com>
2024-09-15 20:02:11 +0300

    improvements

--- resources/views/print.blade.php
+++ resources/views/print.blade.php
@@ -14,23 +14,23 @@
         <div class="flex justify-between px-20 mb-2">
             <div class="mb-4 flex items-center">
                 <p class="pl-2 font-bold">اليوم:</p>
-                <p class=" block w-full rounded-md">{{ \Carbon\Carbon::parse($report->date)->dayName }}</p>
+                <p class=" block w-full rounded-md">{{ $day }}</p>
             </div>
             <div class="mb-4 flex items-center">
                 <p class="pl-2 font-bold">التاريخ:</p>
-                <p class=" w-full rounded-md">{{ $report->date }}</p>
+                <p class=" w-full rounded-md">{{ $date }}</p>
             </div>
             <div class="mb-4 flex items-center">
                 <p class="block pl-2 font-bold">الوقت:</p>
                 <p class=" block w-full rounded-md">
-                    {{ \Carbon\Carbon::parse($report->time)->format('g:i A') }}
+                    {{ $time }}
                 </p>
             </div>
         </div>
         <div class="mb-4">
             <p class="block mb-5 font-bold">شرح الحالة:</p>
             <p class=" block w-full rounded-md">
-                {!! Str::markdown($report->state_description) !!}
+                {!! Str::markdown($description) !!}
             </p>
         </div>
         <div class="mb-2">
@@ -38,7 +38,7 @@
         </div>
         <div class="flex">
             <span class="pl-2">الاسم:</span>
-            <p class=" w-full rounded-md">{{ $report->reporter->name }}</p>
+            <p class=" w-full rounded-md">{{ $reporter }}</p>
         </div>
     </div>
   {{--   <img class="fixed-bottom justify-end mt-20" src={{ asset('image/footer.png') }}> --}}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\DownloadReportController;
 use App\Models\Report;
 use Illuminate\Support\Facades\Route;
 
@@ -11,5 +10,11 @@ Route::get('/', function () {
 Route::get('/print/{report}', function ($report) {
 
     $report = Report::findOrFail($report);
-    return view('print', ['report' => $report]);
+    $day = \Carbon\Carbon::parse($report->date)->dayName;
+    $date = $report->date;
+    $time = \Carbon\Carbon::parse($report->time)->format('g:i A');
+    $description = $report->state_description;
+    $reporter = $report->reporter->name;
+
+    return view('print', compact('day', 'date', 'time', 'reporter', 'description'));
 })->middleware(['auth', 'throttle:60,1'])->name('print');
